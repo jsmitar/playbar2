97536cd3f148e7412a6e664c2cc4c11e87463846
audoban <audoban@openmailbox.org>
2017-06-02 03:12:00 -0500

    Fixed Messages.sh

--- Messages.sh
+++ Messages.sh
@@ -1,53 +1,67 @@
 #!/bin/bash
-echo  ${1:?Root of translatable sources}  ${2:?Project name without .pot extension}
-BASEDIR="$1"                                    # root of translatable sources
-PROJECT="$2"                                    # project name
+
+cd "$(dirname $0)" # root of translatable sources
+BASEDIR="$(pwd)"
+
 BUGADDR="https://github.com/audoban/PlayBar2"	# MSGID-Bugs
-WDIR=`pwd`                                      # working dir
 PACKAGENAME="PlayBar2"
 COPYRIGHT="GPLv3"
 
+function ki18n_xgettext
+{
+    TMPDIR="$BASEDIR/po/tmp"
 
-echo "Preparing rc files"
-cd ${BASEDIR}
-# we use simple sorting to make sure the lines do not jump around too much from system to system
-find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > ${WDIR}/rcfiles.list
-xargs --arg-file=${WDIR}/rcfiles.list extractrc > ${WDIR}/rc.cpp
-# additional string for KAboutData
-#echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> ${WDIR}/rc.cpp
-#echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> ${WDIR}/rc.cpp
-cd ${WDIR}
-echo "Done preparing rc files"
+    WDIR="../../" # working dir
+    PROJECTNAME="$2" # project name
+    TEMPLATE=$3 # desktop template
+    PROJECTPATH="../../$1" # project path
+    PROJECTPATH2= #extra project path
+    TARGET="\e[0;32m$1\e[0m" #target name
 
+    mkdir "$TMPDIR"
+    cd "$TMPDIR"
 
-echo "Extracting messages"
-cd ${BASEDIR}
-# see above on sorting
-find . -name '*.cpp' -o -name '*.h' -o -name '*.c' -o -name '*.qml' | sort > ${WDIR}/infiles.list
-echo "rc.cpp" >> ${WDIR}/infiles.list
-cd ${WDIR}
-xgettext --from-code=UTF-8 -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 \
-    -ki18ncp:1c,2,3 -ktr2i18n:1	-kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale \
-    -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 --msgid-bugs-address="${BUGADDR}" \
-    --copyright-holder="${COPYRIGHT}" --package-name="${PACKAGENAME}" \
-	--files-from=infiles.list -D ${BASEDIR} -D ${WDIR} -L JavaScript -o ${PROJECT}.pot \
-        || { echo "error while calling xgettext. aborting."; exit 1; }
-echo "Done extracting messages"
+    echo -e "-- Preparing rc files for $TARGET"
+    find "${PROJECTPATH}" -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > "${WDIR}/rcfiles.list"
+    xargs --no-run-if-empty --arg-file="${WDIR}/rcfiles.list" extractrc > "${WDIR}/rc.cpp"
+    echo "${WDIR}rc.cpp" > "${WDIR}/infiles.list"
+    echo -e "-- Done preparing rc files for $TARGET"
 
+    echo -e "-- Extracting messages for $TARGET"
+    find "${PROJECTPATH}" -name '*.cpp' -o -name '*.h' -o -name '*.c' -o -name '*.qml' -o -name '*.qml.cmake' | sort >> "${WDIR}/infiles.list"
 
-echo "Merging translations"
-catalogs=`find . -name "${PROJECT}.po"`
-for cat in $catalogs; do
-  echo $cat
-  msgmerge -o $cat.new $cat ${PROJECT}.pot
-  mv $cat.new $cat
-done
-echo "Done merging translations"
+    xgettext --from-code=UTF-8 -L JavaScript -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 \
+    -ktr2i18n:1 -kI18N_NOOP:1 -kI18N_NOOP2:1c,2  -kN_:1 -kaliasLocale -kki18n:1 -kki18nc:1c,2 \
+    -kki18np:1,2 -kki18ncp:1c,2,3 \
+    --msgid-bugs-address="${BUGADDR}" --copyright-holder="${COPYRIGHT}" --package-name="${PACKAGENAME}" \
+    --files-from="${WDIR}/infiles.list" \
+    -D "${TMPDIR}" -o "${WDIR}/${PROJECTNAME}.pot" || \
+    { echo "error while calling xgettext. aborting."; exit 1; }
 
+    #xgettext --from-code=UTF-8 --language=Desktop --join-existing --msgid-bugs-address="${BUGADDR}" \
+    #-k -kName -kGenericName -kComment \
+    #"${WDIR}/" -o "${WDIR}/${PROJECTNAME}.pot" || \
+    #{ echo "error while calling xgettext. aborting."; exit 1; }
 
-echo "Cleaning up"
-cd ${WDIR}
-rm rcfiles.list
-rm infiles.list
-rm rc.cpp
-echo "Done"
+    cd "$WDIR"
+
+    echo -e "-- Merging translations for $TARGET"
+    catalogs=$(find "." -name "${PROJECTNAME}.po")
+    for cat in $catalogs; do
+        echo "${cat}"
+        msgmerge -o "${cat}.new" "${cat}" "${PROJECTNAME}.pot"
+        mv "${cat}.new" "${cat}"
+    done
+    echo -e "-- Done merging translations for $TARGET"
+
+    echo "-- Cleaning up"
+    rm "rcfiles.list"
+    rm "infiles.list"
+    rm "rc.cpp"
+
+    echo -e "-- Done translations for $TARGET\n\n"
+    rm -d "$BASEDIR/po/tmp"
+}
+
+ki18n_xgettext engine   'plasma_engine_playbar'
+ki18n_xgettext plasmoid 'plasma_applet_audoban.applet.playbar'
